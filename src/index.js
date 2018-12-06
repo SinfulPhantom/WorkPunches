f3d41431b84ddd91b7d3914bfffe06241e95fd43
Justin Lafferty <lafferjm@gmail.com>
2018-12-05 18:26:58 -0600

    fixes for physics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import { Game } from 'phaser';
 const gameConfig = {
   width: 680,
   height: 400,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 30 },
+      debug: false
+    }
+  },
   scene: SimpleScene
 };
 
